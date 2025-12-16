d19d0458bc46316958ca828931383d313aa30847
vijay-r <vijaywithluv@yahoo.com>
2025-12-16 12:25:58 +0000

    inti

--- fetch.py
+++ fetch.py
@@ -57,8 +57,8 @@ def fetchXML():
         sorted_data = dict(
             sorted(result_map.items(), key=lambda x: int(x[0]))
         )
-
-        print(f"data={json.dumps(sorted_data, ensure_ascii=False)}")
+    
+    print(f"data={json.dumps(sorted_data, ensure_ascii=False)}")
 
 def main():
     fetchXML()
